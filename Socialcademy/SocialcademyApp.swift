475a6feda34deb5cac8c6ad96d1a1049468b36b7
Ekaterina <anekig@icloud.com>
2025-03-19 14:48:34 +0000

    init

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,23 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Kotya on 19/03/2025.
+//
+
+import Firebase
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    
+    init () {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
