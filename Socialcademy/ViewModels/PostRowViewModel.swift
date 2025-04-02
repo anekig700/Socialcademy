bea6bca58d3e73abec56af299e6a5ce864731c41
Ekaterina <anekig@icloud.com>
2025-04-02 15:43:25 +0100

    authorize users to delete posts

--- Socialcademy/ViewModels/PostRowViewModel.swift
+++ Socialcademy/ViewModels/PostRowViewModel.swift
@@ -16,16 +16,21 @@ class PostRowViewModel: ObservableObject {
     @Published var post: Post
     @Published var error: Error?
     
-    private let deleteAction: Action
+    private let deleteAction: Action?
     private let favoriteAction: Action
     
-    init(post: Post, deleteAction: @escaping Action, favoriteAction: @escaping Action) {
+    init(post: Post, deleteAction: Action?, favoriteAction: @escaping Action) {
         self.post = post
         self.deleteAction = deleteAction
         self.favoriteAction = favoriteAction
     }
     
+    var canDeletePost: Bool { deleteAction != nil }
+    
     func deletePost() {
+        guard let deleteAction = deleteAction else {
+            preconditionFailure("Cannot delete post: no delete action provided")
+        }
         withErrorHandlingTask(perform: deleteAction)
     }
     
