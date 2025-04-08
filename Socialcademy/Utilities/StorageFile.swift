e18a9c8707768d8082f2e6f85cf8a223458d9c39
Ekaterina <anekig@icloud.com>
2025-04-08 10:55:46 +0100

    build the storage file wrapper

---
+++ Socialcademy/Utilities/StorageFile.swift
@@ -0,0 +1,44 @@
+//
+//  StorageFile.swift
+//  Socialcademy
+//
+//  Created by Kotya on 08/04/2025.
+//
+
+import Foundation
+import FirebaseStorage
+
+struct StorageFile {
+    
+    private let storageReference: StorageReference
+    
+    func putFile(form fileURL:URL) async throws -> Self {
+        _ = try await storageReference.putFileAsync(from: fileURL)
+        return self
+    }
+    
+    func getDownloadURL() async throws -> URL {
+        return try await storageReference.downloadURL()
+    }
+    
+    func delete() async throws {
+        try await storageReference.delete()
+    }
+    
+}
+
+extension StorageFile {
+    
+    private static let storage = Storage.storage()
+    
+    static func with(namespace: String, identifier: String) -> StorageFile {
+        let path = "\(namespace)/\(identifier)"
+        let storageReference = storage.reference().child(path)
+        return StorageFile(storageReference: storageReference)
+    }
+    
+    static func atURL(_ downloadURL: URL) -> StorageFile {
+        let storageReference = storage.reference(forURL: downloadURL.absoluteString)
+        return StorageFile(storageReference: storageReference)
+    }
+}
